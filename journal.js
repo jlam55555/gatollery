6e6707382e574b7f3ee278a28b8b176314296be4
Jonathan Lam <jlam55555@gmail.com>
2019-10-06 14:04:56 -0400

    initial commit

---
+++ journal.js
@@ -0,0 +1,8 @@
+const journal = [
+  {
+    file: 'piano_cat.png',
+    title: 'piano cat',
+    description: 'uwu',
+    date: '10 6 19'
+  }
+];
---
+++ load_image.js
@@ -0,0 +1,5 @@
+// get html elems
+const lightbox_img = document.querySelector('#lightbox_img');
+
+// parse url: get url params
+console.log(window.location);
